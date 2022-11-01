7bdb120c29554a9a1a57c4c0e7470d581601ed7d
David Gómez <102187082+dagoalberto600@users.noreply.github.com>
2022-11-01 13:17:53 -0600

    Add files via upload

---
+++ Proyecto.py
@@ -0,0 +1,18 @@
+print("Bievenido a Banco Fide")
+#usuario: Prograadmi contraseña: Ufide1234
+#Informacion de usuario
+user1 = input("Please enter your username: ")
+password1 = input("Please enter your password: ")
+
+#Mensaje de Error mas 3 intentos
+
+user1 = 'Progradmi'
+password1 = 'Ufide1234'
+if user1 == 'Progradmi' and password1 == 'Ufide1234':
+    print("Bienvenido a su cuenta")
+    if user1 != 'Progradmi' and password1 != 'Ufide1234':
+            print("Usuario y/o contraseña incorrecto")
+
+
+
+
